2a248f33ac0caa00c6c32c29904ab6ca0b62bb6b
Okola44 <mercyimaliokola@gmail.com>
2021-02-20 23:11:36 +0300

    assignment

---
+++ src/assignment.kt
@@ -0,0 +1,17 @@
+fun main (){
+    var name = "Happy Ayoma"
+    println(name)
+    var age = 20
+    println(age)
+    var phoneNumber = "0727890499"
+    println(phoneNumber)
+    var weight = "48 kg"
+    println(weight)
+    var citizenship ="Ugandan"
+    println(citizenship)
+    var ugandan :Boolean= true
+    println(Boolean)
+
+
+
+}
